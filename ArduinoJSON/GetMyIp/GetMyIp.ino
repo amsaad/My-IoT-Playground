2b4c0f572aa2504bedf04aa42cdbcb1e676004d6
amsaad <ashraf_sa3d1984@hotmail.com>
2019-12-02 14:12:44 +0300

    Parse the ip value from json and print it

--- ArduinoJSON/GetMyIp/GetMyIp.ino
+++ ArduinoJSON/GetMyIp/GetMyIp.ino
@@ -2,20 +2,20 @@
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
 #include "StringSplitter.h"
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
 
-const char* ssid = "xxxxxx";
-const char* password = "xxxxx";
 WiFiClient client;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting...");
-  }
+  WiFiManager wifiManager;
+  wifiManager.autoConnect();
+  //  while (WiFi.status() != WL_CONNECTED) {
+  //    delay(1000);
+  //    Serial.println("Connecting...");
+  //  }
 }
 
 void loop() {
@@ -25,11 +25,11 @@ void loop() {
 }
 const char* host = "api.ipify.org";
 const int httpPort = 80;
+
 void GetMyIP()
 {
-
   if (!client.connect(host, httpPort)) {
-    Serial.println("connection faillned");
+    Serial.println("connection failled");
   }
   String Request = "GET /?format=json HTTP/1.1\r\n";
   Request += "Host: " + String(host) + "\r\n";
@@ -38,17 +38,14 @@ void GetMyIP()
   client.print(Request);
   String line = client.readString();
   Serial.println("request sent");
-
   String payload = "{";
   StringSplitter *splitter = new StringSplitter(line, '{', 2);
   payload += splitter->getItemAtIndex(1);
-  DynamicJsonDocument doc(128);
-  deserializeJson(doc, payload);
-//  JsonObject root = doc.as<JsonObject>();
-
-
-
+  char json[payload.length() + 1];
+  payload.toCharArray(json, payload.length());;
+  StaticJsonDocument<256> doc;
+  deserializeJson(doc, json);
   String ip = doc["ip"];
   Serial.print("My IP: \t");
-  Serial.println(payload);
+  Serial.println(ip);
 }
